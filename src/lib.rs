17af52d660ff47f938230dcfa2506f193bba4401
John Landahl <john@landahl.org>
2025-09-04 15:55:44 -0700

    Corrected comment about library features

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,13 @@
 //! Online data can come from multiple data sources. Most commonly
 //! a CCP static dump from https://www.fuzzwork.co.uk/dump/.
 //!
-//! The library must be compiled with the apprioriate flags. Currently
-//! accepted flags are `database` and `rpc`. `database` offers a Postgres
-//! backend using the diesel ORM wrapper. `rpc` is for internal use at
-//! the moment as the dependent crate is not open sourced.
+//! The library must be compiled with the appropriate features depending
+//! on the desired backend for universe data. The `postgres` feature
+//! allows for loading from Postgres via the Diesel ORM tool, while the
+//! `sqlite` feature allows loading from a local SQLite file.
+//!
+//! The `rpc` feature is for internal use at the moment as the dependent
+//! crate is not open sourced.
 
 // Must be at the crate root
 #[cfg(feature = "postgres")]
